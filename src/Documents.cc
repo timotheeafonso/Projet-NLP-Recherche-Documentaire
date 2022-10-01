c656678371af1cc560cddcc56c7a613a588ee4ec
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-10-01 20:58:47 +0200

    Corrections

--- src/Documents.cc
+++ src/Documents.cc
@@ -6,6 +6,7 @@
 #include <iterator>
 #include <bits/stdc++.h>
 #include "../lib/rapidxml-1.13/rapidxml.hpp"
+#include <utility>
 #include <vector>
 
 void Documents::print() {
@@ -71,52 +72,49 @@ void Documents::parse(const std::string& path) {
         _documents.push_back(document);
         
     }
-    
-    
 }
 
 std::string Documents::deleteSpecialChar(std::string text) {
+    char specialChar[] = ".,();!?{}\"\"$%€1234567890";
+    int it = 0;
 
-    char specialChar[]=".,();!?{}\"\"$%€1234567890";
-    std::string s=text;
-    int it=0;
-    for(char c : s){
+    for(char c : text){
         it++;
         if (isupper(c)){
             char lower = c + 32;
-            std::replace( s.begin(), s.end(), c,lower);
+            std::replace(text.begin(), text.end(), c, lower);
         }
         for(char c2 : specialChar){
-            if(c==c2){
-                std::replace( s.begin(), s.end(), c,' ');
+            if(c == c2){
+                std::replace(text.begin(), text.end(), c, ' ');
             }
         }
     }
 
-    return s;
+    return text;
 } 
 
-
 std::vector<std::string> Documents::tokenize(std::string text) {
-
-    char delim =' ';
+    char delim = ' ';
     std::vector<std::string> tokens;
-    std::string temp = "";
-    int space=0;
-    for(int i = 0; i < text.length(); i++){
-        if(text[i] == delim){
+    std::string temp;
+    int space = 0;
+
+    for (char i : text) {
+        if(i == delim){
             if(space==0)
                 tokens.push_back(temp);
             temp = "";
             space++;
             
         }
-        else{
-            space=0;
-            if(text[i]!=' ')
-                temp += text[i];    
+        else {
+            space = 0;
+            if (i != ' ')
+                temp += i;
         }       
     }
+
     tokens.push_back(temp);
     return tokens;
 }
@@ -124,29 +122,29 @@ std::vector<std::string> Documents::tokenize(std::string text) {
 
 std::vector<std::string> Documents::getStopword(){
     std::vector<std::string> stopwords;
-    std::ifstream newfile;
-    newfile.open("../stopwords.txt",std::ios::in); //open a file to perform read operation using file object
-    if (newfile.is_open()){ //checking whether the file is open
+    std::ifstream file;
+    file.open("../stopwords.txt", std::ios::in); // Opens a file to perform read operation using file object
+
+    if (file.is_open()){ // Checking whether the file is open
         std::string tp;
-        while (newfile >> tp) {
-            
+        while (file >> tp) {
             stopwords.push_back(tp);
         }
     }
-    newfile.close();
+
+    file.close();
     return stopwords;
 }
 
 std::vector<std::string> Documents::deleteStopwords(std::vector<std::string> tokens, std::vector<std::string> stopwords){
-
-    for(std::vector<std::string>::iterator it = std::begin(tokens); it != std::end(tokens); ++it){
-        for(std::vector<std::string>::iterator it2 = std::begin(stopwords); it2 != std::end(stopwords); ++it2){
-            if(*it==*it2){
+    for(auto it = std::begin(tokens); it != std::end(tokens); ++it) {
+        for(auto it2 = std::begin(stopwords); it2 != std::end(stopwords); ++it2) {
+            if(*it == *it2){
                 tokens.erase(it);
                 it--;
-                
             }
         }
     }
+
     return tokens;
 }
