b767edd71dead5f02d4c26e387bc54bc49fe4cb3
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-10-02 12:58:44 +0200

    Display of sorted words

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include "Document.hh"
 #include "Documents.hh"
+#include "BTree.hh"
+#include "Word.hh"
 
 int main() {
     Documents documents;
     documents.parse("../AP/AP891216");
-    documents.print();
+    // documents.print();
+
+    BTree tree(3);
+    tree.insert(Word("test"));
+    tree.insert(Word("architecte"));
+    tree.insert(Word("pleonasme"));
+    tree.insert(Word("satisfaction"));
+    tree.insert(Word("masks"));
+    tree.insert(Word("power"));
+    tree.insert(Word("original"));
+    tree.insert(Word("zebra"));
+
+    tree.traverse();
+
+    Word word("satisfaction");
+    (tree.search(word) != nullptr)? std::cout << "\nPresent" : std::cout << "\nNot Present";
+
+    Word word_("void");
+    (tree.search(word_) != nullptr)? std::cout << "\nPresent" : std::cout << "\nNot Present";
 
     return 0;
 }
