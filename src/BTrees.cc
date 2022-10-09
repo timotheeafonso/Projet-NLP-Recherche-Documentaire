d03c64eea98ee9722038fd3c2fd11fc58d8b07c5
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-10-09 23:30:31 +0200

    Debug (temp)

--- src/BTrees.cc
+++ src/BTrees.cc
@@ -7,8 +7,23 @@ void BTrees::addTrees(Documents& documents, const int& order) {
         BTree tree(3);
         tree.setNumber(std::move(doc.getNumber()));
 
+        int i = 0;
         for (const auto& word : doc.getContent()) {
+            if(i == 26)
+                break;
             tree.insert(Word(word));
+            //tree.traverse();
+            //std::cout << word;
+            i++;
+        }
+
+        i = 0;
+        for (const auto& word : doc.getContent()) {
+            if(i >= 26) {
+                tree.insert(Word(word));
+                //std::cout << word;
+            }
+            i++;
         }
 
         tree.traverse();
