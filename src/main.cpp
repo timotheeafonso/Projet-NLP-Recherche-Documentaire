d2440cc051206d5668d46425d3867b1ed9066560
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-10-11 20:50:59 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,6 @@
 #include "../build/ui_mainwindow.h"
 
 int main(int argc, char *argv[]) {
-    Documents documents;
-    documents.parse("../AP/AP891216");
-
-    Forest forest;
-    forest.createForest(documents, 3);
-    
-    std::string q="the job of chief of government";
-    Query query(q);
-    query.correctQuery();
-    query.getTopX(forest,10);
-
     QApplication app{argc, argv};
     MainWindow ui;
     ui.show();
